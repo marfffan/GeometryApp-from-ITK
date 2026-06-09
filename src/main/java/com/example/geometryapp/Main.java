0fe18677a03a77985b4229e40f1f8283ca67f03a
marfffan <120277929+marfffan@users.noreply.github.com>
2026-06-09 14:27:19 +0300

    обновить GeometryApp для использования всех модулей библиотеки
    - Обновлён pom.xml: добавлены зависимости на:
      * geometry-core (1.1.0)
      * geometry-utils (1.0.0-SNAPSHOT)
      * ThreeDimensionalShapes (1.0.0-SNAPSHOT)
    - Обновлён Main.java: добавлена демонстрация:
      * 2D фигур (круг, прямоугольник, треугольник)
      * Утилиты (конвертация 150 см в метры)
      * 3D фигур (куб, сфера)
    - Приложение успешно собрано и запущено

--- src/main/java/com/example/geometryapp/Main.java
+++ src/main/java/com/example/geometryapp/Main.java
@@ -3,24 +3,32 @@ package com.example.geometryapp;
 import com.example.geometry.Circle;
 import com.example.geometry.Rectangle;
 import com.example.geometry.Triangle;
+import com.example.geometryutils.GeometryUtils;
+import com.example.threedimensionalshapes.Cube;
+import com.example.threedimensionalshapes.Sphere;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Геометрические фигуры ===");
+        System.out.println("=== Геометрические фигуры ===\n");
 
+        System.out.println("--- 2D Фигуры ---");
         Circle circle = new Circle(5);
-        System.out.println("Круг:");
-        System.out.println("  Площадь: " + circle.getArea());
-        System.out.println("  Периметр: " + circle.getPerimeter());
+        System.out.println("Круг: площадь = " + circle.getArea());
 
         Rectangle rectangle = new Rectangle(4, 7);
-        System.out.println("Прямоугольник:");
-        System.out.println("  Площадь: " + rectangle.getArea());
-        System.out.println("  Периметр: " + rectangle.getPerimeter());
+        System.out.println("Прямоугольник: площадь = " + rectangle.getArea());
 
         Triangle triangle = new Triangle(3, 4, 5);
-        System.out.println("Треугольник:");
-        System.out.println("  Площадь: " + triangle.getArea());
-        System.out.println("  Периметр: " + triangle.getPerimeter());
+        System.out.println("Треугольник: площадь = " + triangle.getArea());
+
+        System.out.println("\n--- Утилиты ---");
+        System.out.println("150 см = " + GeometryUtils.cmToMeters(150) + " метров");
+
+        System.out.println("\n--- 3D Фигуры ---");
+        Cube cube = new Cube(3);
+        System.out.println("Куб: объём = " + cube.getVolume());
+
+        Sphere sphere = new Sphere(2);
+        System.out.println("Сфера: объём = " + sphere.getVolume());
     }
 }
